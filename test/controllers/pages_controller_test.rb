4419f3837b3639adcae4de204d9de05030e1394c
ROGELIOGALDAMEZ <rogelio.galdamez@gmail.com>
2014-09-29 11:44:05 +0930

    added a site layout

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,14 +1,42 @@
 require 'test_helper'
 
+describe PagesController do
+  render_views
+end
+
 class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+  end
+  
+  test "should have the right title" do
+    get :home
+    response.should have_selector("Title", :content => "Ruby on Rails Tut Sample App | Home")
+  end
+  
+  test "should have a non-blank body" do
+    get :home
+    response.body.should_not =~ /<body>\s*<\/body>/
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
   end
+  
+   test "should have the right title" do
+    get :contact
+    response.should have_selector("Title", :content => "Ruby on Rails Tut Sample App | Contact")
+  
+  test "should get contact" do
+    get :about
+    assert_response :success
+    
+     test "should have the right title" do
+    get :about
+    response.should have_selector("Title", :content => "Ruby on Rails Tut Sample App | About")
+  end
+
 
 end
