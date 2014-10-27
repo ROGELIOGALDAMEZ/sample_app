706fc39c9976de0e89bef1bad00da674987f7fb2
ROGELIOGALDAMEZ <rogelio.galdamez@gmail.com>
2014-10-27 15:32:18 +1030

    implemented @current_user functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,16 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.authenticate(params[:sessions][:email], params[:sessions][:password])
     
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'  
+    else
+      sign_in user
+      redirect_to user_path(user)
+    end
   end
   
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -42,12 +42,18 @@ class User < ActiveRecord::Base
     
     def authenticate(email, submitted_password)
       user = find_by_email(email)
-      return nil  if user.nil?
-      return user if user.has_password?(submitted_password)
+      (user && user.has_password?(submitted_password)) ? user : nil
     end
+    
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
+    
   end
   
   private
+  
     def encrypt_password
         self.salt = make_salt if new_record?
         self.encrypted_password = encrypt(self.password)
